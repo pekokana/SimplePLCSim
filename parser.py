2f23ea655bc94239766c4030b62b4add7fa9eb15
pekokana <zeropfac@gmail.com>
2025-12-21 22:20:05 +0900

    reset

--- parser.py
+++
@@ -1,5 +0,0 @@
-def parse_rung(rung_str):
-    coil = rung_str.split("--(")[1].replace(")", "").strip()
-    logic = rung_str.split("--(")[0].replace("[", "").replace("]", "").strip()
-    logic = logic.replace("AND", "and").replace("OR", "or").replace("NOT", "not")
-    return {"logic": logic, "coil": coil}
